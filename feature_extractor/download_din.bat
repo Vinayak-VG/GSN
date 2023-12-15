300966df5b9cee7511147ec93261fd57643921c0
Vinayak-VG <vinayakguptapokal@gmail.com>
2023-12-15 13:35:12 +0530

    webpage

--- feature_extractor/download_din.bat
+++
@@ -1,3 +0,0 @@
-curl https://dl.fbaipublicfiles.com/dino/dino_vitbase8_pretrain/dino_vitbase8_pretrain.pth
-mkdir ckpts
-mv dino_vitbase8_pretrain.pth ckpts/
--- feature_extractor/run.sh
+++
@@ -1,4 +0,0 @@
-python3 -W ignore extract_llff.py
-python3 -W ignore extract_ibrnet.py
-python3 -W ignore extract_ibrnet.py --main_folder ../../../../../media/Data1/vinayak/ibrnet_collected_2 --images_name images_8
-python3 -W ignore extract_realiconic.py
